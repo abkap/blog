4d897d586222a29047a0b98666aa16927694f229
Abhishek <apabhishek2002@gmail.com>
2021-09-22 18:08:14 +0530

    added empty container if search result is emtpy

--- public/js/search.js
+++ public/js/search.js
@@ -5,9 +5,22 @@ var articleEach = document.querySelectorAll(".article-each"); // article contain
 var aboutSection = document.querySelector(".info");
 var hr = document.querySelector(".hr");
 var value;
+var isArticlesEmtpy;
+
+const articles = document.querySelector(".articles");
+const div = document.createElement("div");
+const h3 = document.createElement("h3");
+div.classList.add("article-each");
+div.classList.add("emtpy-articles");
+h3.classList.add("article-title");
+h3.textContent = "No Similar Posts Found !";
+div.appendChild(h3);
+articles.appendChild(div);
 
 function searchItem() {
   value = input.value.toLowerCase();
+  isArticlesEmtpy = true;
+  div.style.display = "none";
 
   if (value.length > 0) {
     aboutSection.style.display = "none";
@@ -25,10 +38,18 @@ function searchItem() {
     ) {
       // found the item
       articleEach[i].style.display = "";
+      isArticlesEmtpy = false;
     } else {
       // item not found
 
       articleEach[i].style.display = "none";
     }
   }
+
+  if (isArticlesEmtpy) {
+    // show emty tag
+    div.style.display = "";
+    console.log("article is empty");
+  }
+  // console.log("executed serach");
 }
